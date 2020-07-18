f9e19b2a0112aa0a9aa36ec7e9625ad01807e701
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-07-17 20:31:40 -0600

    ex2.py done
    All tests passed, comments added for each line

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,30 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Instantiate the ticket_origination dictionary
+    ticket_origination = {}
 
-    return route
+    # Create a for loop the iterates through all the tickets
+    for ticket in tickets:
+        # Set the ticket.source to ticket
+        ticket_origination[ticket.source] = ticket
+
+    # Set cur_ticket to index with the key of "NONE"
+    cur_ticket = ticket_origination["NONE"]
+    # Create the array for the "overly_complicated_python_function_to_figure_out_your_route" array
+    overly_complicated_python_function_to_figure_out_your_route = []
+
+    # Run a for loop for the length of the tickets_organization array
+    while True:
+        # Add the cur_ticket's destination to the overly_complicated_python_function_to_figure_out_your_route array
+        overly_complicated_python_function_to_figure_out_your_route.append(cur_ticket.destination)
+        # If the tickets ending destination is "NONE" (the last ticket)
+        if cur_ticket.destination == "NONE":
+            # End the loop
+            break
+        # Set the current ticket to be the value of the index of cur_ticket within the ticket_organization array
+        cur_ticket = ticket_origination[cur_ticket.destination]
+
+    # Return the array
+    print(overly_complicated_python_function_to_figure_out_your_route)
+    return overly_complicated_python_function_to_figure_out_your_route
