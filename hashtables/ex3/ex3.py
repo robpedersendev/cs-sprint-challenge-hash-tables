e9a9ca3462cd280cd9b482174e887291074ebb6e
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-07-17 21:52:34 -0600

    ex3.py done
    All tests passed, comments added for each line

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,36 @@
+# Build a helper function to check to see if the duplicate value exists
+def does_duplicate_exist(value, dicts):
+    # Loop through all dictionaries
+    for dict in dicts:
+        # The the value provided is not in the dict
+        if value not in dict:
+            # Return false
+            return False
+    # Otherwise return True
+    return True
+
+
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Create the array to hold all unique values
+    unique_array = []
+
+    # Loop through the arrays
+    for array in arrays:
+        # Create a unique values dictionary
+        one_offs = {}
+        # For each individual value in the array
+        for value in array:
+            # For each value, set it to equal true
+            one_offs[value] = True
+        # Add each one_offs dictionary to the unique_array
+        unique_array.append(one_offs)
+    final = unique_array[-1]
+    # Create a result array
+    result = []
+
+    for value in final:
+        if does_duplicate_exist(value, unique_array[:-1]):
+            result.append(value)
 
     return result
 
